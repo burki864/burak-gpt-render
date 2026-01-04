e97ecf98aed0305b50f273bead0f9ff6119ff15d
burki864 <burakerenkisapro1122@gmail.com>
2026-01-04 21:02:24 +0300

    Update email.py

--- backend/email.py
+++ backend/email.py
@@ -1,1 +1,17 @@
+import smtplib
+from email.message import EmailMessage
+import os
 
+EMAIL = "burakgpthelpcenter@gmail.com"
+PASSWORD = os.getenv("EMAIL_PASSWORD")
+
+def send_2fa(email: str, code: str):
+    msg = EmailMessage()
+    msg["Subject"] = "BurakGPT Doğrulama Kodu"
+    msg["From"] = EMAIL
+    msg["To"] = email
+    msg.set_content(f"Giriş doğrulama kodunuz: {code}")
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(EMAIL, PASSWORD)
+        server.send_message(msg)
