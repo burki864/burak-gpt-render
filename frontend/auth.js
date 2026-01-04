4535b104a98c02d2812847cd90c10a1032498d0f
burki864 <burakerenkisapro1122@gmail.com>
2026-01-04 21:04:05 +0300

    Update auth.js

--- frontend/auth.js
+++ frontend/auth.js
@@ -1,1 +1,36 @@
+let mode = "login";
+
+function openLogin() {
+    mode = "login";
+    document.getElementById("modal-title").innerText = "Giriş Yap";
+    document.getElementById("switch").innerHTML =
+        "Hesabın yok mu? <a onclick='openSignup()'>Kayıt Ol</a>";
+    document.getElementById("modal").classList.remove("hidden");
+}
+
+function openSignup() {
+    mode = "signup";
+    document.getElementById("modal-title").innerText = "Kayıt Ol";
+    document.getElementById("switch").innerHTML =
+        "Zaten hesabın var mı? <a onclick='openLogin()'>Giriş Yap</a>";
+    document.getElementById("modal").classList.remove("hidden");
+}
+
+function closeModal() {
+    document.getElementById("modal").classList.add("hidden");
+}
+
+async function submitAuth() {
+    const email = email.value;
+    const password = password.value;
+
+    const endpoint = mode === "login" ? "/auth/login" : "/auth/signup";
+
+    const res = await fetch(endpoint + `?email=${email}&password=${password}`, {
+        method: "POST"
+    });
+
+    const data = await res.json();
+    alert(JSON.stringify(data));
+}
 
